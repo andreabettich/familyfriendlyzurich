fb30b8e62b0cc222a0cfda642bcf7472be178397
Andrea Bettich <andrea.bettich@namics.com>
2013-10-31 00:01:59 +0100

    Made the javascript a bit nicer Added other geojson files. Added assets for the map

--- js/familyfriendly.js
+++ js/familyfriendly.js
@@ -1,23 +1,55 @@
+
 $(function() {
-	 var map;
-	var ajaxRequest;
-	var plotlist;
-	var plotlayers=[];
+	FamilyFriendly.initmap();
+});
 
-	var geojsonFeature;
-	function initmap() {
-		// set up the map
-		map = new L.Map('map');
-
+var FamilyFriendly = { 
+	map : {},
+	initmap : function () {	
+		map = new L.Map('map');	
 		// create the tile layer with correct attribution
 		var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 		var osmAttrib='Map data © OpenStreetMap contributors';
-		var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});		
+		var osm = new L.TileLayer(osmUrl, {minZoom: 12, maxZoom: 18, attribution: osmAttrib});		
 
 		// start the map in South-East England
 		map.setView(new L.LatLng(47.3717,8.5359),12);
-		map.addLayer(osm);		
+		map.addLayer(osm);
+		FamilyFriendly.initMapData();
+	},
+	initMapData: function(){
+		FamilyFriendly.loadMigros();
+		FamilyFriendly.loadKinderkrippe();
+		FamilyFriendly.loadKindergarten();
+	},
+	loadMigros: function(){
+		 var migrosIcon = L.icon({
+		    iconUrl: 'assets/migros.png',
+		});
 
+	    $.getJSON("json/migros.json", function(data) {
+	        var points = L.geoJson(data.features, {
+	            pointToLayer: function(feature, latlng) {
+	                return new L.marker(latlng, {icon: migrosIcon}).addTo(map);
+	            }
+	        });
+	        points.addTo(map);
+	    });
+	},
+	loadKinderkrippe: function(){
+		var clusters2 = new L.MarkerClusterGroup({showCoverageOnHover: false, zoomToBoundsOnClick:true});
+	    $.getJSON("json/kinderkrippe.json", function(data) {
+	        var points = L.geoJson(data.features, {
+	            pointToLayer: function(feature, latlng) {
+	                var marker = L.marker(latlng);
+	                clusters2.addLayer(marker);
+	                return clusters2;
+	            }
+	        });
+	    });
+	    clusters2.addTo(map);
+	},
+	loadKindergarten: function(){
 		var clusters = new L.MarkerClusterGroup({showCoverageOnHover: false, zoomToBoundsOnClick:true});
 
 	    $.getJSON("json/kindergarden.json", function(data) {
@@ -30,29 +62,5 @@ $(function() {
 	        });
 	    });
 	    clusters.addTo(map);
-
-	   	var clusters2 = new L.MarkerClusterGroup({showCoverageOnHover: false, zoomToBoundsOnClick:true});
-
-	    $.getJSON("json/kinderkrippe.json", function(data) {
-	        var points = L.geoJson(data.features, {
-	            pointToLayer: function(feature, latlng) {
-	                var marker = L.marker(latlng);
-	                clusters2.addLayer(marker);
-	                return clusters2;
-	            }
-	        });
-	    });
-	    clusters2.addTo(map);	
 	}
-
-	function loadGeoJson(){
-		$.get('json/kindergarden.json', function (data) {
-			alert(data);
-			geojsonFeature = data;	
-		});
-
-		alert(geojsonFeature);
-	}
-	//loadGeoJson();
-	initmap();
-});
+}
---
+++ json/magic.py
@@ -0,0 +1,6 @@
+import json
+import elasticsearch
+
+es = elasticsearch.Elasticsearch()
+
+
